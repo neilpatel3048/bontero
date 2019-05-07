8d6cee7757f2d478ae16b060381724cb3071cd22
Neil Patel <neilpatel3048@gmail.com>
2019-05-07 19:39:28 +0200

    feat: new category feature api

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,36 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+ exports.categories = functions.https.onRequest((request, response) => {
+
+    const categories = [{
+        "category_id": "CAT001",
+        "category_photo": "url",
+        "category_name": "Organic Produce"
+      },
+      
+      {
+        "category_id": "CAT002",
+        "category_photo": "url",
+        "category_name": "Drinks and Dairy"
+      },
+      
+      {
+        "category_id": "CAT003",
+        "category_photo": "url",
+        "category_name": "Dry Goods"
+      },
+      
+      {
+        "category_id": "CAT004",
+        "category_photo": "url",
+        "category_name": "Home and Beauty"
+      },
+      
+      {
+        "category_id": "CAT005",
+        "category_photo": "url",
+        "category_name": "Sweets"
+      }]
+ response.send("helolo");
+ });
