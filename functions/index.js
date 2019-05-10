9a1484ddab1c4a362848f05ce7f3c79682b42e14
Gael Merino-Donahue <merinogael@gmail.com>
2019-05-10 20:02:24 +0200

    Update-Blog_Categories-Search

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const blog = require('./apis/blogs');
+const categories = require('./apis/categories');
+
+module.exports = {
+    getBlog: functions.https.onRequest(blog.get),
+    searchBlog: functions.https.onRequest(blog.search),
+    getCategories: functions.https.onRequest(categories.get),
+    searchCategories: functions.https.onRequest(categories.search)
+};
