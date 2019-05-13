0eabacf3d06b495b3de800f1e8e6a04452e764f9
Romulo Cintra <romulocintra@gmail.com>
2019-05-13 18:06:30 +0200

    products

--- functions/api/products.js
+++ functions/api/products.js
@@ -1,32 +1,22 @@
-var admin = require("firebase-admin")
+var admin = require('firebase-admin');
 
-var db= admin.database();
-var ref = db.ref("products");
+var db = admin.database();
+var ref = db.ref('products');
 
 const getFn = (request, response) => {
-    ref.orderByChild("product_id")
-    .on("value", (data) => {
-        response.send(data.val());
-    });
-
-
-    };
-
-
-const search = (req, res) => {
-    const search = req.body.search;
-    ref.orderByChild("product_name").equalTo(search).on("value", (data) => {
-        res.send(data.val());
-  });
+	ref.orderByChild('product_id').on('value', (data) => {
+		response.send(data.val());
+	});
 };
 
-
-
-
+const search = (req, res) => {
+	const search = req.body.search;
+	ref.orderByChild('product_name').equalTo(search).on('value', (data) => {
+		res.send(data.val());
+	});
+};
 
 module.exports = {
-get : getFn,
-search : search
-
-
-}
+	get: getFn,
+	search: search
+};
