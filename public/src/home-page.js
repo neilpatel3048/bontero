5945d1ae39eccbacfb0ca65a7fd1b9cd926e9e17
Gael Merino-Donahue <merinogael@gmail.com>
2019-05-27 21:33:34 +0200

    Added Home and Product Desc HTML in index

--- public/src/home-page.js
+++ public/src/home-page.js
@@ -6,7 +6,165 @@ class HomePage extends HTMLElement {
     }
     connectedCallback(){
         console.log(`Heyyy I'm Home Page!!!`);
-        this.innerHTML = `Heyyy HOLA`
+        this.innerHTML = `        <ion-content>
+        <div class="hero-image">
+    <div class="hero-text">
+      <h1>Be the Change you want to see</h1>
+      <p>Shop Responsibly</p>
+      <p>Act with love</p>
+      <p>Change the world</p>
+    </div>
+  </div>
+        <div>    
+        <p>
+          A new ecommerce and lifestyle platform for the eco-conscious
+        </p>
+        </div>
+        
+        <div class="container">
+    <img src="https://cdn.glitch.com/62308f07-7e5c-4e0b-b423-e20d18091413%2Ffresh%20produce.jpg?1557852582588" style="width:100%;">
+    <div class="text-block">
+      <h4>Shop our products</h4>
+      <ion-icon name="arrow-dropright"></ion-icon>
+    </div>
+  </div>
+        
+            <div class="container">
+              <img src="https://cdn.glitch.com/62308f07-7e5c-4e0b-b423-e20d18091413%2Fvolunteer.jpg?1558100516328" style="width:100%;">
+              <div class="text-block"> 
+      <h4>Volunteer at local event</h4>
+    </div>
+  </div>
+        
+              <div class="container">
+                <img src="https://cdn.glitch.com/62308f07-7e5c-4e0b-b423-e20d18091413%2Fblog_recycle.jpg?1558100706884" style="width:100%;">
+                <div class="text-block"> 
+      <h4>Read our blog</h4>
+    </div>
+  </div>
+        
+        <div>    
+        <p>
+  Sign up to get discounts, rewards, and be part of the eco community here.       
+        </p>
+        </div>
+        
+        <div class="container">
+          <img src="https://cdn.glitch.com/62308f07-7e5c-4e0b-b423-e20d18091413%2Fseed.jpg?1558102672861" style="width:100%;">
+          <div class="text-block"> 
+      <h4>Our mission and vision</h4>
+    </div>
+  </div>
+        
+        <div>
+          <h2>
+            Featured Products
+          </h2>
+        </div>
+  
+ <!-------------Grid starts here-------------------------------------------------------------------------------------------------------->
+   <ion-grid>
+      
+    <ion-row>
+      <ion-col size="6">
+        <ion-card class="cardpadding">
+      <img class= "imagesize" src="https://firebasestorage.googleapis.com/v0/b/bontero-50f6a.appspot.com/o/avocado.jpg?alt=media&token=6a141a17-7c6b-4b86-a44d-9ebc1b2dbaa9" />
+      <ion-card-header class="productname">
+    
+        <ion-card-title>Avocado</ion-card-title>
+      </ion-card-header>
+         
+      <ion-card-content class="cancelprice">
+        € 02.10       
+      </ion-card-content>
+      <ion-card-content class="lastprice">
+        € 1.50       
+      </ion-card-content>
+        
+  
+          
+    </ion-card>
+      </ion-col>
+      
+       <ion-col size="6">
+        <ion-card class="cardpadding">
+      <img class="imagesize" src="https://firebasestorage.googleapis.com/v0/b/bontero-50f6a.appspot.com/o/onions.png?alt=media&token=59663134-a7b3-4f8f-94b6-d4cbf28bf64a" />
+     <ion-card-header class="productname">
+    
+        <ion-card-title>Avocado</ion-card-title>
+      </ion-card-header>
+         
+      <ion-card-content class="cancelprice">
+        € 3.10 
+      </ion-card-content>
+        <ion-card-content class="lastprice">
+        € 2.10       
+       </ion-card-content>
+    
+          </ion-card>
+      </ion-col>
+   
+    </ion-row>
+     
+    </ion-grid> 
+ 
+        
+        <div>
+          <h2>
+            Featured Events
+          </h2>
+        </div>
+  
+     <ion-grid>
+          
+        <ion-row>
+          <ion-col size="6">
+            <ion-card class="cardpadding">
+          <img class= "imagesize" src="https://firebasestorage.googleapis.com/v0/b/bontero-50f6a.appspot.com/o/avocado.jpg?alt=media&token=6a141a17-7c6b-4b86-a44d-9ebc1b2dbaa9" />
+          <ion-card-header class="productname">
+        
+            <ion-card-title>Clean Beach</ion-card-title>
+          </ion-card-header>
+             
+          <ion-card-content class="date">
+           May 10 
+          </ion-card-content>
+          <ion-card-content class="place">
+           Cituadella
+          </ion-card-content>
+            
+      
+              
+        </ion-card>
+          </ion-col>
+          
+           <ion-col size="6">
+            <ion-card class="cardpadding">
+          <img class="imagesize" src="https://firebasestorage.googleapis.com/v0/b/bontero-50f6a.appspot.com/o/Group%205.png?alt=media&token=2daf90be-2798-4f79-b338-0cbae2fd1983" />
+         <ion-card-header class="productname">
+        
+            <ion-card-title>Plant trees</ion-card-title>
+          </ion-card-header>
+             
+          <ion-card-content class="date">
+            June 1
+          </ion-card-content>
+          <ion-card-content class="place">
+           Gracia
+          </ion-card-content>
+        </ion-card>
+          </ion-col>
+       
+        </ion-row>
+         
+        </ion-grid> 
+        
+        
+        <div>
+          <h2>
+            Featured Events
+          </h2>
+        </div>`
     }
 }
 
--- public/src/productdescription-page.js
+++ public/src/productdescription-page.js
@@ -4,7 +4,67 @@ class ProductdescriptionPage extends HTMLElement {
     }
     connectedCallback(){
         console.log(`Heyyy I'm Product Page!!!`);
-        this.innerHTML = `Heyyy HOLA`
+        this.innerHTML = `    <ion-content >
+      
+       
+        <!-- Back arrow . organic produce header and sort icon-->
+         
+        <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-icon class="backarrow" ios="ios-arrow-back" md="md-arrow-back"></ion-icon>
+          </ion-buttons>
+          <ion-title class="orgheader">Avocado</ion-title>
+          <ion-buttons slot="end">
+            <ion-icon class="sort" ios="ios-funnel" md="md-funnel"></ion-icon>
+          </ion-buttons>
+        </ion-toolbar>
+            
+            
+            
+            <!-- Content starts here-->
+            <div class="productdesc">
+      
+              <ion-card class="imagesizepd">
+                <img class= "imagesize" src="https://firebasestorage.googleapis.com/v0/b/bontero-50f6a.appspot.com/o/avocado.jpg?alt=media&token=6a141a17-7c6b-4b86-a44d-9ebc1b2dbaa9" />
+              </ion-card>
+                <div class= "pdproductdetail">
+                  <h1 class="pname"> Avocado</h1>
+                  <center>
+                  <p class="pdproductname"><span style="text-decoration: line-through;">  2,00&euro; &nbsp;</span> 1,25&euro; 37% OFF</p>
+                  <p>&nbsp;</p>
+                    </center>
+                </div>
+              <center>
+              <ion-button class="btn_quantity" color="success">-</ion-button>
+                <input type="text" name="quantity" value="1" align="center">
+                <ion-button class="btn_quantity" color="success">+</ion-button>
+                <ion-button class="btn_add" color="success">ADD</ion-button>
+            
+                </center>
+        <!-- Content end here-->
+             </div>      
+            
+          <!-- impact footprint description--> 
+            <ion-toolbar>
+              <ion-buttons slot="start" class="btndesc">
+                Description
+              </ion-buttons>
+                <center>
+                  <label class="btnfootprint" align="center">Footprint</label>  <!--hereeeeee   ion segments-->
+                
+                </center>
+              <ion-buttons slot="end" class="btnproducer">
+               Producer
+              </ion-buttons>
+           </ion-toolbar>
+        
+              
+              <!-- description--> 
+            <p class="desccontent">
+            Grown in the heartland of Spain, free from pesticides and other harmful chemicals. The Hass avocado contains many nutrients and is considered one of the greatest superfoods on Earth. It’s a perfect snack or addition to a meal that will keep your body happy and healthy. 
+              <br><br>
+              Produced in: Malaga, Spain by La Nucia Farms
+            </p>`
     }
 }
 
