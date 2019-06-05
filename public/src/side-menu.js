83bc69c8494353fe9cdcb80f4c3e320c98bbd2fe
Gael Merino-Donahue <merinogael@gmail.com>
2019-06-05 18:46:41 +0200

    Menu bar fixed with fonts and colors on brand

--- public/src/side-menu.js
+++ public/src/side-menu.js
@@ -11,44 +11,44 @@ class SideMenu extends HTMLElement {
 	connectedCallback() {
 		const css = `
 		<style>
-		  .sidenav {
+			.sidenav {
 			height: 100%;
 			width: 0;
 			position: fixed;
 			z-index: 1;
 			top: 0;
 			left: 0;
-			background-color: #111;
+			background-color: #FFDD80;
 			overflow-x: hidden;
 			transition: 0.5s;
 			padding-top: 60px;
-		  }
+			font-family: Avenir;
+		}
 		  
-		  .sidenav a {
+			.sidenav a {
 			padding: 8px 8px 8px 32px;
 			text-decoration: none;
 			font-size: 25px;
-			color: #818181;
+			color: #000000;
 			display: block;
 			transition: 0.3s;
-		  }
+		}
 		  
-		  .sidenav a:hover {
-			color: #f1f1f1;
-		  }
+			.sidenav a:hover {
+			color: #009E71;
+		}
 		  
-		  .sidenav .closebtn {
+			.sidenav .closebtn {
 			position: absolute;
 			top: 0;
 			right: 25px;
 			font-size: 36px;
 			margin-left: 50px;
-		  }
+		}
 		  
-		  @media screen and (max-height: 450px) {
+			@media screen and (max-height: 450px) {
 			.sidenav {padding-top: 15px;}
 			.sidenav a {font-size: 18px;}
-		  }
 		 </style> 
 		  `;
 
